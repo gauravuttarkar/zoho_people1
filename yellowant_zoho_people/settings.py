34b4e5f745a997fb2af256fb51170171ab70b65b
Gaurav <gaurav.uttarkar@gmail.com>
2018-07-02 16:17:51 +0530

    Changes for heroku

--- yellowant_zoho_people/settings.py
+++ yellowant_zoho_people/settings.py
@@ -156,20 +156,7 @@ else:
         }
     }
 
-DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'zoho_people1',
-        'USER': "root",
-        'PASSWORD': "root1234",
-        'HOST': "localhost",
-        'PORT': '',
-    }
-}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
