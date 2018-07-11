c79ffa9bfff07dcf42413b88bd1977a996a22f99
Gaurav <gaurav.uttarkar@gmail.com>
2018-07-11 11:17:42 +0530

    removed

--- lib/yellowant_command_center/commands_by_invoke_name.py
+++ lib/yellowant_command_center/commands_by_invoke_name.py
@@ -1,12 +1,12 @@
 """Mapping for command invoke name to logic"""
-from .commands import fetch_all_employees, get_employee, add_employee, get_attendance
+from .commands import fetch_all_employees, get_employee, add_employee
 
 
 COMMANDS_BY_INVOKE_NAME = {
     "fetchallemployees": fetch_all_employees,
     "getemployee": get_employee,
     "addemployee": add_employee,
-    "getattendance": get_attendance
+    #"getattendance": get_attendance
 
 
 
